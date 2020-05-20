6270ef005e139f0005316b51dca2b11964dfe769
WilliamL-R <32361704+WilliamL-R@users.noreply.github.com>
2020-05-21 00:08:40 +0100

    Added SpotifyConnection object functionality to SpotifyConnection.kt

--- app/src/main/java/com/wlr/spotitrack/connection/SpotifyConnection.kt
+++ app/src/main/java/com/wlr/spotitrack/connection/SpotifyConnection.kt
@@ -1,4 +1,37 @@
 package com.wlr.spotitrack.connection
 
+import android.content.Context
+import android.util.Log
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
+
 object SpotifyConnection {
+    private const val CLIENT_ID = "Trying To find a way to deal with using this securely lol."
+    private const val  REDIRECT_URI = "com.wlr.spotifyplayer://callback"
+
+    private var spotifyAppRemote : SpotifyAppRemote? = null
+    private var connectionParams: ConnectionParams = ConnectionParams.Builder(CLIENT_ID)
+        .setRedirectUri(REDIRECT_URI)
+        .showAuthView(true)
+        .build()
+
+    fun connect(context: Context, handler: (connected: Boolean) -> Unit){
+        if (spotifyAppRemote?.isConnected == true){
+            handler(true)
+            return
+        }
+        val connectionListener = object : Connector.ConnectionListener {
+            override fun onConnected(p0: SpotifyAppRemote?) {
+                this@SpotifyConnection.spotifyAppRemote
+                handler(true)
+            }
+
+            override fun onFailure(throwable: Throwable?) {
+                Log.e("SpotifyConnection", throwable?.message, throwable)
+                handler(false)
+            }
+        }
+        SpotifyAppRemote.connect(context, connectionParams, connectionListener)
+    }
 }
